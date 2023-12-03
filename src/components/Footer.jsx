7802433388c3418796728b28d9ae3e1b6d5aadca
Kacper <kacperrebosz@gmail.com>
2023-12-03 20:04:27 +0100

    footer-update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function PageFooter() {
   return (
     <>
       <div className="w-full h-12 bg-[#054b28] text-white flex-shrink-0 flex items-center justify-center shadow-2xl 2xl:mt-20">
-        <h1 className="text-center text-[1rem] sm:text-xl 2xl:text-2xl 3xl:text-3xl">
+        <h1 className="text-center text-[1rem] 2xl:text-xl 3xl:text-2xl">
           &copy;2023 Rodzinny Ogród Działkowy MAGNOLIA Kielce | Projekt: Kacper
           Rębosz
         </h1>
